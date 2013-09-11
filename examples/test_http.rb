2d89a5cc08ecea96455c6f4c3e1d694b8d3e3930
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-10 22:21:57 -0700

    Update example using http

--- examples/test_http.rb
+++ examples/test_http.rb
@@ -4,7 +4,7 @@ Carnivore.configure do
   s = Carnivore::Source.build(:type => :http, :args => {:bind => '0.0.0.0', :port => 3000})
 
   s.add_callback(:printer) do |message|
-    info "GOT MESSAGE: #{message[:message].body} - source: #{message[:source]} - instance: #{self}"
+    info "GOT MESSAGE: #{message[:message]} - source: #{message[:source]} - instance: #{self}"
   end
 
 end.start!
