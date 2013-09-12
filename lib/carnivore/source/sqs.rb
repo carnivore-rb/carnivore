139ee59114986179a1b59e51437007d25dd83064
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-12 16:08:23 -0700

    Update SQS to allow multiple queues to pull messages

--- lib/carnivore/source/sqs.rb
+++ lib/carnivore/source/sqs.rb
@@ -5,14 +5,14 @@ module Carnivore
   class Source
     class Sqs < Source
 
-      OUTPUT_REPEAT_EVERY=5
+      OUTPUT_REPEAT_EVERY=20
 
       attr_reader :pause_time
 
       def setup(args={})
         @fog = nil
         @connection_args = args[:fog]
-        @queue = args[:queue_url]
+        @queues = Array(args[:queues]).compact.flatten
         @pause_time = args[:pause] || 5
         @receive_timeout = after(args[:receive_timeout] || 30){ terminate }
         debug "Creating SQS source instance <#{name}>"
@@ -24,13 +24,15 @@ module Carnivore
 
       def receive(n=1)
         count = 0
-        m = nil
-        until(m)
-          m = nil
+        msgs = []
+        while(msgs.empty?)
+          msgs = []
           @receive_timeout.reset
-          m = @fog.receive_message(@queue, 'MaxNumberOfMessages' => n).body['Message'].first
+          msgs = @queues.map do |q|
+            @fog.receive_message(q, 'MaxNumberOfMessages' => n).body['Message']
+          end.flatten.compact
           @receive_timeout.reset
-          unless(m)
+          if(msgs.empty?)
             if(count == 0)
               debug "Source<#{name}> no message received. Sleeping for #{pause_time} seconds."
             elsif(count % OUTPUT_REPEAT_EVERY == 0)
@@ -40,7 +42,7 @@ module Carnivore
           end
           count += 1
         end
-        pre_process(m)
+        msgs.map{|m| pre_process(m) }
       end
 
       def send(message)
