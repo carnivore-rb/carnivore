a6091bc03f103b15ef5c03a0471e54072a0f4855
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-27 15:33:15 -0700

    Clean up syntax to make more readable

--- lib/carnivore/supervisor.rb
+++ lib/carnivore/supervisor.rb
@@ -2,7 +2,11 @@ module Carnivore
   class Supervisor < Celluloid::SupervisionGroup
 
     Source.sources.each do |source|
-      supervise source.klass, as: source.source_hash[:name], args: [source.source_hash]
+      supervise(
+        source.klass,
+        as: source.source_hash[:name],
+        args: [source.source_hash]
+      )
     end
 
   end
