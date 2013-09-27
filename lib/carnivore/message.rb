1a5231b3f9fe55f90e858e0fdb1f16aeea84c0a6
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-27 15:34:42 -0700

    Provide custom type for message (mainly for nicer logging)

---
+++ lib/carnivore/message.rb
@@ -0,0 +1,22 @@
+module Carnivore
+  class Message
+
+    attr_reader :args
+
+    def initialize(args={})
+      @args = args
+    end
+
+    def [](k)
+      @args[k.to_sym] || @args[k.to_s]
+    end
+
+    def inspect
+      "<Carnivore::Message[#{self.object_id}] @args=#{args}>"
+    end
+
+    def to_s
+      "<Carnivore::Message:#{self.object_id}>"
+    end
+  end
+end
