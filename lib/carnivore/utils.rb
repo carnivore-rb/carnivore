4d1dcf7cef6993e66a4e6f774ad99b8be96f4467
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-27 15:33:01 -0700

    Add common module for logging to utils

--- lib/carnivore/utils.rb
+++ lib/carnivore/utils.rb
@@ -14,5 +14,23 @@ module Carnivore
       end
     end
 
+    module Logging
+
+      %w(debug info warn error).each do |key|
+        define_method(key) do |string|
+          log(key, string)
+        end
+      end
+
+      def log(*args)
+        if(args.empty?)
+          Celluloid::Logger
+        else
+          severity, string = args
+          Celluloid::Logger.send(severity.to_sym, "#{self}: #{string}")
+        end
+      end
+
+    end
   end
 end
