96a411a7b7800bb57c5bcd62692824c60dd2acad
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-27 15:34:08 -0700

    Make automatic response optional. Pass connection instance allong with message.

--- lib/carnivore/source/http.rb
+++ lib/carnivore/source/http.rb
@@ -15,7 +15,8 @@ module Carnivore
       def default_args(args)
         {
           :bind => '0.0.0.0',
-          :port => '3000'
+          :port => '3000',
+          :auto_respond => true
         }.merge(args)
       end
 
@@ -23,13 +24,13 @@ module Carnivore
         srv = Reel::Server.supervise(args[:bind], args[:port]) do |con|
           while(req = con.request)
             begin
-              msg = format(:request => req, :body => req.body)
+              msg = format(:request => req, :body => req.body, :connection => con)
               callbacks.each do |name|
                 c_name = callback_name(name)
                 debug "Dispatching message<#{msg[:message].object_id}> to callback<#{name} (#{c_name})>"
                 Celluloid::Actor[c_name].async.call(msg)
               end
-              con.respond(:ok, 'So long, and thanks for all the fish!')
+              con.respond(:ok, 'So long, and thanks for all the fish!') if args[:auto_respond]
             rescue => e
               con.respond(:bad_request, 'Failed to process request')
             end
