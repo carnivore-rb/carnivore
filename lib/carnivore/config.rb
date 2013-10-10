0dac87e4e91ed37e6abd1880dc77b6dad543609d
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-09 19:00:04 -0700

    Add some docs to the methods

--- lib/carnivore/config.rb
+++ lib/carnivore/config.rb
@@ -8,12 +8,16 @@ module Carnivore
 
     class << self
 
+      # args:: configuration hash
+      # Merge provided args into configuration
       def configure(args)
         build(args[:config_path]) if args[:config_path]
         self.merge!(args)
         self
       end
 
+      # path_or_hash:: Path to JSON file or configuration Hash
+      # Populates the configuration
       def build(path_or_hash)
         if(path_or_hash.is_a?(Hash))
           conf = path_or_hash
@@ -31,6 +35,14 @@ module Carnivore
         self
       end
 
+      # ary: keys into a hash
+      # Returns value if exists or nil
+      #  Example:
+      #    Config.build(:my_app => {:port => 30})
+      #    Config.get(:my_app, :port) => 30
+      #    Config.get(:my_app, :host) => nil
+      #    Config.get(:other_app, :port) => nil
+      #    Config.get(:my_app, :mail, :server) => nil
       def get(*ary)
         ary.flatten.inject(self) do |memo, key|
           memo[key.to_s] || memo[key.to_sym] || break
