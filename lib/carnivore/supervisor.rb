b595acc7a4c2c574c79c4fc95df7ad9196085fbe
Chris Roberts <chrisroberts.code@gmail.com>
2014-03-06 15:09:03 -0800

    Only restart member if actor exists

--- lib/carnivore/supervisor.rb
+++ lib/carnivore/supervisor.rb
@@ -43,7 +43,7 @@ module Carnivore
       unless(instance)
         if(member = @members.detect{|m| m && m.name.to_s == name.to_s})
           Celluloid::Logger.warn "Found missing actor in member list. Attempting to restart manually."
-          restart_actor(member.actor, true)
+          member.restart if member.actor
           instance = @registry[name]
           unless(instance)
             Celluloid::Logger.error "Actor restart failed to make it available in the registry! (#{name})"
